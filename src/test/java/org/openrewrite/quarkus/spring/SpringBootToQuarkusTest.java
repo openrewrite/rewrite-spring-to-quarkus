ef5321974f9580bf78bc36c8454d49fc130150b3
Tim te Beek <tim@moderne.io>
2025-10-10 23:24:25 +0200

    Fix two of the failing tests

--- src/test/java/org/openrewrite/quarkus/spring/SpringBootToQuarkusTest.java
+++ src/test/java/org/openrewrite/quarkus/spring/SpringBootToQuarkusTest.java
@@ -37,64 +37,72 @@ class SpringBootToQuarkusTest implements RewriteTest {
     @Test
     void migrateSpringBootParentToQuarkusBOM() {
         rewriteRun(
-          //language=xml
-          pomXml(
-            """
-              <project>
-                  <parent>
-                      <groupId>org.springframework.boot</groupId>
-                      <artifactId>spring-boot-starter-parent</artifactId>
-                      <version>3.1.0</version>
-                  </parent>
-                  <groupId>com.example</groupId>
-                  <artifactId>demo</artifactId>
-                  <version>0.0.1-SNAPSHOT</version>
-                  <properties>
-                      <java.version>17</java.version>
-                  </properties>
-                  <dependencies>
-                      <dependency>
-                          <groupId>org.springframework.boot</groupId>
-                          <artifactId>spring-boot-starter-web</artifactId>
-                      </dependency>
-                  </dependencies>
-              </project>
-              """,
-            spec -> spec.after(actual -> {
-                Matcher versionMatcher = Pattern.compile("<artifactId>quarkus-bom</artifactId>\\s*<version>(3\\.[^<]+)</version>").matcher(actual);
-                assertThat(versionMatcher.find()).isTrue();
-                String quarkusVersion = versionMatcher.group(1);
-                return """
-                  <project>
-                      <groupId>com.example</groupId>
-                      <artifactId>demo</artifactId>
-                      <version>0.0.1-SNAPSHOT</version>
-                      <properties>
-                          <java.version>17</java.version>
-                      </properties>
-                      <dependencyManagement>
-                          <dependencies>
-                              <dependency>
-                                  <groupId>io.quarkus.platform</groupId>
-                                  <artifactId>quarkus-bom</artifactId>
-                                  <version>%s</version>
-                                  <type>pom</type>
-                                  <scope>import</scope>
-                              </dependency>
-                          </dependencies>
-                      </dependencyManagement>
-                      <build>
-                          <plugins>
-                              <plugin>
-                                  <groupId>io.quarkus.platform</groupId>
-                                  <artifactId>quarkus-maven-plugin</artifactId>
-                                  <version>%s</version>
-                              </plugin>
-                          </plugins>
-                      </build>
-                  </project>
-                  """.formatted(quarkusVersion, quarkusVersion);
-            })
+          mavenProject("project",
+            //language=xml
+            pomXml(
+              """
+                <project>
+                    <parent>
+                        <groupId>org.springframework.boot</groupId>
+                        <artifactId>spring-boot-starter-parent</artifactId>
+                        <version>3.1.0</version>
+                    </parent>
+                    <groupId>com.example</groupId>
+                    <artifactId>demo</artifactId>
+                    <version>0.0.1-SNAPSHOT</version>
+                    <properties>
+                        <java.version>17</java.version>
+                    </properties>
+                    <dependencies>
+                        <dependency>
+                            <groupId>org.springframework.boot</groupId>
+                            <artifactId>spring-boot-starter-web</artifactId>
+                        </dependency>
+                    </dependencies>
+                </project>
+                """,
+              spec -> spec.after(actual -> {
+                  Matcher versionMatcher = Pattern.compile("<artifactId>quarkus-bom</artifactId>\\s*<version>(3\\.[^<]+)</version>").matcher(actual);
+                  assertThat(versionMatcher.find()).isTrue();
+                  String quarkusVersion = versionMatcher.group(1);
+                  return """
+                    <project>
+                        <groupId>com.example</groupId>
+                        <artifactId>demo</artifactId>
+                        <version>0.0.1-SNAPSHOT</version>
+                        <properties>
+                            <java.version>17</java.version>
+                        </properties>
+                        <dependencyManagement>
+                            <dependencies>
+                                <dependency>
+                                    <groupId>io.quarkus.platform</groupId>
+                                    <artifactId>quarkus-bom</artifactId>
+                                    <version>%s</version>
+                                    <type>pom</type>
+                                    <scope>import</scope>
+                                </dependency>
+                            </dependencies>
+                        </dependencyManagement>
+                        <dependencies>
+                            <dependency>
+                                <groupId>io.quarkus</groupId>
+                                <artifactId>quarkus-resteasy-jackson</artifactId>
+                            </dependency>
+                        </dependencies>
+                        <build>
+                            <plugins>
+                                <plugin>
+                                    <groupId>io.quarkus.platform</groupId>
+                                    <artifactId>quarkus-maven-plugin</artifactId>
+                                    <version>%s</version>
+                                </plugin>
+                            </plugins>
+                        </build>
+                    </project>
+                    """.formatted(quarkusVersion, quarkusVersion);
+              })
+            )
           )
         );
     }
@@ -102,76 +110,84 @@ class SpringBootToQuarkusTest implements RewriteTest {
     @Test
     void migrateSpringBootWithExistingDependencyManagement() {
         rewriteRun(
-          //language=xml
-          pomXml(
-            """
-              <project>
-                  <parent>
-                      <groupId>org.springframework.boot</groupId>
-                      <artifactId>spring-boot-starter-parent</artifactId>
-                      <version>3.1.0</version>
-                  </parent>
-                  <groupId>com.example</groupId>
-                  <artifactId>demo</artifactId>
-                  <version>0.0.1-SNAPSHOT</version>
-                  <dependencyManagement>
-                      <dependencies>
-                          <dependency>
-                              <groupId>com.fasterxml.jackson</groupId>
-                              <artifactId>jackson-bom</artifactId>
-                              <version>2.15.2</version>
-                              <type>pom</type>
-                              <scope>import</scope>
-                          </dependency>
-                      </dependencies>
-                  </dependencyManagement>
-                  <dependencies>
-                      <dependency>
-                          <groupId>org.springframework.boot</groupId>
-                          <artifactId>spring-boot-starter-web</artifactId>
-                      </dependency>
-                  </dependencies>
-              </project>
-              """,
-            spec -> spec.after(actual -> {
-                Matcher versionMatcher = Pattern.compile("<artifactId>quarkus-bom</artifactId>\\s*<version>(3\\.[^<]+)</version>").matcher(actual);
-                assertThat(versionMatcher.find()).isTrue();
-                String quarkusVersion = versionMatcher.group(1);
-                return """
-                  <project>
-                      <groupId>com.example</groupId>
-                      <artifactId>demo</artifactId>
-                      <version>0.0.1-SNAPSHOT</version>
-                      <dependencyManagement>
-                          <dependencies>
-                              <dependency>
-                                  <groupId>com.fasterxml.jackson</groupId>
-                                  <artifactId>jackson-bom</artifactId>
-                                  <version>2.15.2</version>
-                                  <type>pom</type>
-                                  <scope>import</scope>
-                              </dependency>
-                              <dependency>
-                                  <groupId>io.quarkus.platform</groupId>
-                                  <artifactId>quarkus-bom</artifactId>
-                                  <version>%s</version>
-                                  <type>pom</type>
-                                  <scope>import</scope>
-                              </dependency>
-                          </dependencies>
-                      </dependencyManagement>
-                      <build>
-                          <plugins>
-                              <plugin>
-                                  <groupId>io.quarkus.platform</groupId>
-                                  <artifactId>quarkus-maven-plugin</artifactId>
-                                  <version>%s</version>
-                              </plugin>
-                          </plugins>
-                      </build>
-                  </project>
-                  """.formatted(quarkusVersion, quarkusVersion);
-            })
+          mavenProject("project",
+            //language=xml
+            pomXml(
+              """
+                <project>
+                    <parent>
+                        <groupId>org.springframework.boot</groupId>
+                        <artifactId>spring-boot-starter-parent</artifactId>
+                        <version>3.1.0</version>
+                    </parent>
+                    <groupId>com.example</groupId>
+                    <artifactId>demo</artifactId>
+                    <version>0.0.1-SNAPSHOT</version>
+                    <dependencyManagement>
+                        <dependencies>
+                            <dependency>
+                                <groupId>com.fasterxml.jackson</groupId>
+                                <artifactId>jackson-bom</artifactId>
+                                <version>2.15.2</version>
+                                <type>pom</type>
+                                <scope>import</scope>
+                            </dependency>
+                        </dependencies>
+                    </dependencyManagement>
+                    <dependencies>
+                        <dependency>
+                            <groupId>org.springframework.boot</groupId>
+                            <artifactId>spring-boot-starter-web</artifactId>
+                        </dependency>
+                    </dependencies>
+                </project>
+                """,
+              spec -> spec.after(actual -> {
+                  Matcher versionMatcher = Pattern.compile("<artifactId>quarkus-bom</artifactId>\\s*<version>(3\\.[^<]+)</version>").matcher(actual);
+                  assertThat(versionMatcher.find()).isTrue();
+                  String quarkusVersion = versionMatcher.group(1);
+                  return """
+                    <project>
+                        <groupId>com.example</groupId>
+                        <artifactId>demo</artifactId>
+                        <version>0.0.1-SNAPSHOT</version>
+                        <dependencyManagement>
+                            <dependencies>
+                                <dependency>
+                                    <groupId>com.fasterxml.jackson</groupId>
+                                    <artifactId>jackson-bom</artifactId>
+                                    <version>2.15.2</version>
+                                    <type>pom</type>
+                                    <scope>import</scope>
+                                </dependency>
+                                <dependency>
+                                    <groupId>io.quarkus.platform</groupId>
+                                    <artifactId>quarkus-bom</artifactId>
+                                    <version>%s</version>
+                                    <type>pom</type>
+                                    <scope>import</scope>
+                                </dependency>
+                            </dependencies>
+                        </dependencyManagement>
+                        <dependencies>
+                            <dependency>
+                                <groupId>io.quarkus</groupId>
+                                <artifactId>quarkus-resteasy-jackson</artifactId>
+                            </dependency>
+                        </dependencies>
+                        <build>
+                            <plugins>
+                                <plugin>
+                                    <groupId>io.quarkus.platform</groupId>
+                                    <artifactId>quarkus-maven-plugin</artifactId>
+                                    <version>%s</version>
+                                </plugin>
+                            </plugins>
+                        </build>
+                    </project>
+                    """.formatted(quarkusVersion, quarkusVersion);
+              })
+            )
           )
         );
     }
